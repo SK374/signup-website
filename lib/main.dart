8bdc80cb3d9cc93a989b77096da6c9a1360a349a
Sakari <sk374@protonmail.com>
2021-04-02 22:11:57 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -22,11 +22,3 @@ class SignUpApp extends StatelessWidget {
     );
   }
 }
-
-// Add scrolling to make singe page website
-
-// Hosting has been added to Firebase. Isuee with setting up with custom domain. 123Reg has been updated but all settings
-// need to be rechecked as site is not working live.
-
-// Name of the project needs to be changed to gymprove_website. Can't move to new project as Firebase web hosing
-// files are in this project or will need to re do that too. See my book for notes on Firebase hosting.
